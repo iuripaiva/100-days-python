d3278dea40ce12ebccb54d76c543e19b85331141
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2023-12-15 15:08:00 +0000

    add more lessons from other days.

--- day1.py
+++ day1.py
@@ -3,13 +3,19 @@
 # Função print()
 # DICA: Pra pular linha usando somente um comando de print, em vez de ficar usando vários, é só usar
 # o termo "\n" no meio da string.
-# Manipulação de strings (concatenação )
-# Debugging iniciante: Atenção a Typos (erros tipográficos/erros de escrita)
-# Função input()
+# Manipulação de strings (concatenação ).
+
+# Debugging iniciante: Atenção a Typos (erros tipográficos/erros de escrita).
+
+# Função input().
+
 # DICA: Pra imprimir uma resposta a um input usando uma só linha de código, dá pra fazer da forma abaixo:
-# print("Olá, " + input("Qual é o seu nome? ") + "!")
-# O resultado impresso será "Olá, {seu nome}!"
+    # print("Olá, " + input("Qual é o seu nome? ") + "!")
+    # O resultado impresso será "Olá, {seu nome}!"
+
 # Variáveis
+
+# DESAFIO FINAL DO DIA 1:
 
 print("Hello! This is the Band Name Generator.\n")
 
---
+++ day4.py
@@ -0,0 +1,23 @@
+# Termos aprendidos durante o day 4: 
+
+# Aleatorização: Uso da biblioteca random (random.randint() e random.random()).
+    # random.randint(a, b): Gera um número aleatório dentro do intervalo de a até b, e inclui os dois valores também.
+    # random.random(): Gera um numero de ponto flutuante (float) no intervalo de 0.0 a 0.1, mas não inclui o 1.0.
+
+# Importação e exportação de códigos: Usando import de outro arquivo criado anteriormente e também importando a biblioteca random.
+
+# Python Lists: 
+    # Exemplo de lista: fruits = ["Apple", "Banana", "Orange"]
+    # Exemplo de consulta a essa lista: print(fruits[1]) ==> O resultado será Banana.
+    # DICA: Listas iniciam a contagem a partir do 0, assim como praticamente tudo na programação. Porém, se você buscar algo numa lista iniciando por -1, ela será contada de trás pra frente.
+        # DICA ex: print(fruits[-1]) ==> O resultado será Orange.
+    # DICA2: Pra mudar um valor dentro de uma lista, basta selecionar o index dela e alterar o valor
+        # DICA2 ex: fruits[2] = "Cherry" ==> Isso vai substituir a fruta no index 2 (Orange) por Cherry.
+    # DICA3: Pra adicionar um novo dado na lista, usa-se a função "append()".
+        # DICA3 ex: fruits.append("Pear") ==> Vai adicionar uma nova posição no final da lista, com o valor "Pear".
+
+    # DICA MASTER: NÃO memorize todas as funções da lista ou algo do tipo, você só precisa saber o que precisa e buscar na documentação pra implementar. CONSULTE, não MEMORIZE.
+
+
+# DESAFIO FINAL DO DIA 4:
+
