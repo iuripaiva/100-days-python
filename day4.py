39821deb6b02342d8de40d109d529b26bbe4a692
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2023-12-17 02:45:04 +0000

    Added day 5

--- day4.py
+++ day4.py
@@ -7,17 +7,82 @@
 # Importação e exportação de códigos: Usando import de outro arquivo criado anteriormente e também importando a biblioteca random.
 
 # Python Lists: 
-    # Exemplo de lista: fruits = ["Apple", "Banana", "Orange"]
-    # Exemplo de consulta a essa lista: print(fruits[1]) ==> O resultado será Banana.
-    # DICA: Listas iniciam a contagem a partir do 0, assim como praticamente tudo na programação. Porém, se você buscar algo numa lista iniciando por -1, ela será contada de trás pra frente.
-        # DICA ex: print(fruits[-1]) ==> O resultado será Orange.
-    # DICA2: Pra mudar um valor dentro de uma lista, basta selecionar o index dela e alterar o valor
-        # DICA2 ex: fruits[2] = "Cherry" ==> Isso vai substituir a fruta no index 2 (Orange) por Cherry.
-    # DICA3: Pra adicionar um novo dado na lista, usa-se a função "append()".
-        # DICA3 ex: fruits.append("Pear") ==> Vai adicionar uma nova posição no final da lista, com o valor "Pear".
+    # Regular Lists:
+        # Exemplo de lista: fruits = ["Apple", "Banana", "Orange"]
+        # Exemplo de consulta a essa lista: print(fruits[1]) ==> O resultado será Banana.
+        # DICA: Listas iniciam a contagem a partir do 0, assim como praticamente tudo na programação. Porém, se você buscar algo numa lista iniciando por -1, ela será contada de trás pra frente.
+            # DICA ex: print(fruits[-1]) ==> O resultado será Orange.
+        # DICA2: Pra mudar um valor dentro de uma lista, basta selecionar o index dela e alterar o valor
+            # DICA2 ex: fruits[2] = "Cherry" ==> Isso vai substituir a fruta no index 2 (Orange) por Cherry.
+        # DICA3: Pra adicionar um novo dado na lista, usa-se a função "append()".
+            # DICA3 ex: fruits.append("Pear") ==> Vai adicionar uma nova posição no final da lista, com o valor "Pear".
+    # Nested Lists:
+        # Exemplo de nested list: food = [["apple", "strawberry", "banana", "pear"],["lettuce", "cabbage", "broccoli", "spinach"]]
+            # Na lista acima existem 2 listas dentro de uma principal, separando as comidas por frutas e verduras.
 
     # DICA MASTER: NÃO memorize todas as funções da lista ou algo do tipo, você só precisa saber o que precisa e buscar na documentação pra implementar. CONSULTE, não MEMORIZE.
 
 
 # DESAFIO FINAL DO DIA 4:
 
+import random as r
+
+rock = """
+Rock
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+paper = """
+Paper
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissors = """
+Scissors
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+game_img = [rock, paper, scissors]
+
+print("Welcome to the Rock, Paper, Scissors game!\n")
+
+computer_choice = r.randint(0,2)
+player_choice = int(input("Choose 0 for rock, 1 for paper or 2 for scissors: "))
+
+# INVALID NUMBER
+if player_choice < 3 and player_choice > -1: # OPTIONS ONLY GO FROM 0 TO 2
+
+    print(f"\nYou chose:")
+    print(game_img[player_choice])
+
+    print(f"The computer chose:")
+    print(game_img[computer_choice])
+
+    # WINS AND LOSSES
+    if player_choice == 0 and computer_choice == 2: # ROCK (0) BEATS SCISSORS (2)
+        print("You win.")
+    elif computer_choice == 0 and player_choice == 2: # ROCK (0) BEATS SCISSORS (2)
+        print("You lose.")
+    elif player_choice < computer_choice:  # THE SMALLER NUMBER ALWAYS LOSES (EXCEPT ABOVE CONDITIONS)
+        print("You lose.")
+    elif player_choice > computer_choice: # THE BIGGER NUMBER ALWAYS WINS (EXCEPT ABOVE CONDITIONS)
+        print("You win.")
+    # DRAW
+    elif player_choice == computer_choice:
+        print("It's a draw.")
+
+else:
+    print("Invalid number.")
---
+++ day5.py
@@ -0,0 +1,51 @@
+# Termos aprendidos durante o day 5: 
+
+# Loops:
+
+# for loop:
+#   fruits = ["Apple", "Peach", "Pear"]
+#   for fruit in fruits:
+#       print(fruit)
+
+# for ... in range:
+#   total = 0
+#   for number in range (0, 100):
+#       total += number
+#   print(total)
+
+# DICA: O numero final do range não conta com ele mesmo, ex: Se usar um range de 1 a 100, só vai contar do 1 até o 99. O melhor nesse caso é fazer de 0 a 100 ou de 1 a 101.
+
+# DESAFIO FINAL DO DIA 5:
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password:\n"))
+nr_symbols = int(input("How many symbols would you like in your password:\n"))
+nr_numbers = int(input("How many numbers would you like in your password:\n"))
+
+password = ""
+
+for letter in range (0, nr_letters):
+    password += random.choice(letters) # .choice() é uma função da biblioteca random que retorna um elemento escolhido aleatoriamente de uma determinada sequencia, como a lista "letters" nesse caso.
+
+for symbol in range (0, nr_symbols):
+    password += random.choice(symbols)
+
+for number in range (0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
+
+password_list = list(password) # Cria uma lista, separando cada caractere da variavel "password" em indices e armazena na variavel "password_list"
+
+random.shuffle(password_list) # Usa a função .shuffle() pra embaralhar a lista armazenada na variavel "password_list"
+
+password = ''.join(password_list) # Junta toda a lista em uma string só e sobrescreve a variavel "password" inicial
+
+print(f"Here is your password: {password}")
