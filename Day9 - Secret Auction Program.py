982aec44e04b751c6c531abf000cc55cd6c8858f
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2023-12-29 04:57:43 -0300

    add day 9

---
+++ Day9 - Secret Auction Program.py
@@ -0,0 +1,24 @@
+import secret_auction_art as s_art
+
+bidders = {}
+
+print(s_art.logo)
+print("Welcome to the Secret Auction program.")
+
+def new_bidder():
+    bd_name = input("\nWhat is your name? ")
+    bd_bid = int(input("\nWhat's your bid? $"))
+    bidders[bd_name] = bd_bid
+    answer = input("\nAre there any other users to bid? Type 'yes' or 'no': ")
+    if answer.lower() == "yes":
+        new_bidder()
+    else:
+        biggest_bid = 0
+        biggest_bidder = ""
+        for key, value in bidders.items():
+            if biggest_bid < value:
+                biggest_bid = value
+                biggest_bidder = key
+        print(f"The winner is {biggest_bidder} with ${biggest_bid}!")
+
+new_bidder()
